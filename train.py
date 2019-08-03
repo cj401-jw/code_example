72dcaa312a2f403111c356d1af9733b1a04b507f
linkyeu <45722581+linkyeu@users.noreply.github.com>
2019-08-03 16:15:29 +0300

    Add files via upload
    Reworked logging with TensorBoard. But status of training still could be found in terminal.

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import utils
 from model import data_loader, net, metric
 from evaluate import evaluate
 from pathlib import Path
+from torch.utils.tensorboard import SummaryWriter
 
 # parse input data
 parser = argparse.ArgumentParser()
@@ -15,7 +16,7 @@ parser.add_argument('--restore_file', default=None,
 
 def prepare_source_file(model_dir):
     """Load source *.py file with architecture code which located
-    in /model_dir. This folder should contain only one *.py file.
+    in /model_dir. This folder should contsain only one *.py file.
     Inputs:
     - model_dir: (string) we use this to find source *.py file
       with architecture.
@@ -35,26 +36,18 @@ def prepare_source_file(model_dir):
     
     return source
 
+
 def train(model, optimizer, scheduler, loss_fn, dataloader, metrics, params):
     """Train the model on `num_steps` batches (e.g. one epoch).
     
-    NOTE: If we need to check new architecture needs to run train function with limited 
-          number of batched. Just to check that model able to overfit on these data.
-          Here we need to decrese variable 't', e.g. t = int(t * 0.3), if want to train
-          over 30% of dataset.
-          
     Inputs:
     - metrics: (dict) a dictionary of functions that compute a metric using the output and labels of each batch;
     - params: (Params) hyperparameters."""
-    torch.cuda.benchmark=True
     
     model.train()                 # drop out and batch norm in train mode
     torch.set_grad_enabled(True)  # calculate grads     
     
-    summ = []                         # summary for current train loop
-    loss_avg = utils.RunningAverage() # running avg object for loss
-
-    # use tqdm for progress bar    
+    # use tqdm for progress bar
     t = tqdm.tqdm(dataloader)
     for i, (images, labels) in enumerate(t): 
         images, labels = images.to(device), labels.to(device)
@@ -72,27 +65,17 @@ def train(model, optimizer, scheduler, loss_fn, dataloader, metrics, params):
         scheduler.step()
         
         # evaluate model once per save_summary_steps
-        if i % params.save_summary_steps == 0:
-            # extract data from torch Variable -> move to cpu -> numpy arrays
-            scores = scores.detach().cpu().numpy()
+        if i % params.save_summary_steps == 0:           
+            # calculate metrics with numpy variables 
+            scores = scores.detach().cspu().numpy()
             labels = labels.detach().cpu().numpy()
-            
             summary_batch = {metric: metrics[metric](scores, labels) for metric in metrics}
-            summary_batch['loss'] = loss.item()
-            summ.append(summary_batch)
-
-        # update the average loss
-        loss_avg.update(loss.item())
-        # TODO: add all metrics ploting
-        t.set_postfix(loss='{:05.3f}'.format(loss_avg()), refresh=False)
-       
-    # compute mean of all metrics in summary, e.g. for epoch
-    metrics_mean = {metric: np.mean([x[metric] for x in summ]) for metric in summ[0]}
-    metrics_string = " ; ".join("{}: {:05.3f}".format(k, v) for k, v in metrics_mean.items())
-    logging.info("- Train metrics: " + metrics_string)
-    
-    return {'train': metrics_mean}
-
+            
+            # logging all metrcis with TensorBoard
+            writer.add_scalar(tag='loss', scalar_value=loss.item(), global_step=None)
+            for metric in summary_batch:
+                writer.add_scalar(tag=metric.key(), scalar_value=metric.value(), global_step=None)
+            
 
 def train_and_evaluate(model, train_dataloader, val_dataloader, optimizer, scheduler, monitor_metric, 
                        loss_fn, metrics, params, model_dir, restore_file=None):
@@ -110,40 +93,21 @@ def train_and_evaluate(model, train_dataloader, val_dataloader, optimizer, sched
         logging.info("Restoring parameters from {}".format(restore_path))
         utils.load_checkpoint(restore_path, model, optimizer)
 
-    best_val_acc = 0.0
-    
     # early stopping 
+    best_val_acc = 0.0
     early_stopping = params.early_stopping   
     assert type(early_stopping) == dict, "early_stopping variable should be a dict."
     callback = utils.EarlyStopping(optimizer, monitor_metric=early_stopping['monitor_metric'], 
                                    mode=early_stopping['mode'], patience=early_stopping['patience'])
-    
-    # init history dict 
-    assert type(metrics) == dict, "Metrics variable is not a dict."
-    list_of_metircs = list(metrics.keys()) + ['loss']
-    history = {'train' : {k: [] for k in list_of_metircs}, 'valid': {k: [] for k in list_of_metircs}}
-    
-    # init visualization
-    rt_monitoring = utils.RealTimeMonitoring(history)
-    # allows to run script without closing figure & give a time to load empty plot
-    plt.show(block=False); plt.pause(0.1)
-            
-    
+    # run one epoch
     for epoch in range(params.num_epochs):
-        # run one epoch
-        logging.info("Epoch {}/{}".format(epoch + 1, params.num_epochs))
-
+        
         # compute number of batches in one epoch and save to history
-        train_metrics = train(model, optimizer, scheduler, loss_fn, train_dataloader, metrics, params)
-        for metric in list_of_metircs:  history['train'][metric].append(train_metrics['train'][metric])
+        train(model, optimizer, scheduler, loss_fn, train_dataloader, metrics, params)
         
         # evaluate and write to history for one epoch on validation set
         val_metrics = evaluate(model, loss_fn, val_dataloader, metrics, params) 
-        for metric in list_of_metircs:  history['valid'][metric].append(val_metrics[metric])
-            
-        # plot step
-        rt_monitoring.update(history)
-            
+                       
         # track early stopping call back
         stop_training = callback.update(val_metrics)   
         pat = early_stopping['patience']
@@ -151,10 +115,9 @@ def train_and_evaluate(model, train_dataloader, val_dataloader, optimizer, sched
             logging.info(f"- Early Stopping: val {monitor_metric} doesn't improve {pat} epochs. Stop training.")
             break  
                
+        # save weights
         val_acc = val_metrics['accuracy']
         is_best = val_acc>=best_val_acc
-
-        # save weights
         utils.save_checkpoint({'epoch': epoch + 1,
                                'state_dict': model.state_dict(),
                                'optim_dict' : optimizer.state_dict()},
@@ -164,31 +127,14 @@ def train_and_evaluate(model, train_dataloader, val_dataloader, optimizer, sched
         # if best_eval, best_save_path
         if is_best:
             logging.info("- Found new best accuracy")
-            best_val_acc = val_acc
+            best_val_acc = val_acc      
 
-            # Save best val metrics in a json file in the model directory
-            best_json_path = os.path.join(model_dir, "metrics_val_best_weights.json")
-            utils.save_dict_to_json(val_metrics, best_json_path)
-
-        # Save latest val metrics in a json file in the model directory
-        last_json_path = os.path.join(model_dir, "metrics_val_last_weights.json")
-        utils.save_dict_to_json(val_metrics, last_json_path)
-    
-    plt.close()
-    
-    # save history to json in model's directory
-    history_json_path = os.path.join(model_dir, "history.json")
-    with open(history_json_path, 'w') as file:
-        json.dump(history, file, indent=4)
-        
-    # save plot of model training history to the model folder
-    utils.save_model_history_graphics(history, model_dir)
-    
 
 if __name__ == '__main__':
     
-    # release torch memory if filled
-    torch.cuda.empty_cache()
+    # Writer will output to ./runs/ directory by default
+    # TODO: chanhe root directory
+    writer = SummaryWriter()
 
     # Load the parameters from json file
     args = parser.parse_args()
@@ -198,6 +144,8 @@ if __name__ == '__main__':
     
     # use GPU if available
     params.cuda = torch.cuda.is_available()
+    torch.cuda.benchmark=True
+    torch.cuda.empty_cache()
 
     # Set the random seed for reproducible experiments
     torch.manual_seed(230)
@@ -270,3 +218,5 @@ if __name__ == '__main__':
         model_dir=args.model_dir, 
         restore_file=args.restore_file,
         )
+    
+    writer.close()
